389e5df6c437f1c678813d862b6e29cda7f2d96a
Mauricio Bayas <X018328@avidxchange.com>
2023-10-11 15:35:07 -0400

    Second Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,17 @@ import { WikipediaService } from './wikipedia.service';
 })
 export class AppComponent {
 
+  pages = [];
+
   constructor(private wikipedia: WikipediaService) {}
 
   onSubmittedTerm = (term: string) => {
     // const results = this.wikipedia.search(term);
     // console.log('I am calling from the App File', term);
     // console.log(results);
-    this.wikipedia.search(term).subscribe((response) => {
-      console.log(response);
+    this.wikipedia.search(term).subscribe((response: any) => {
+      // console.log(response);
+      this.pages = response.query.search;
     })
   }
 }
